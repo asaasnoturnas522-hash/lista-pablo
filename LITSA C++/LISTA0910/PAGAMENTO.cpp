73c64c33d1d80f629f9720a05f828a6c05a0039a
asaasnoturnas522-hash <asaasnoturnas522@gmail.com>
2025-10-14 18:23:04 -0300

    Create PAGAMENTO.cpp

---
+++ LITSA C++/LISTA0910/PAGAMENTO.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+    string nome;
+    double valorHora, horas, pagamento;
+
+    cout << "Nome do funcionario: ";
+    getline(cin, nome);
+    cout << "Valor por hora: ";
+    cin >> valorHora;
+    cout << "Horas trabalhadas: ";
+    cin >> horas;
+
+    pagamento = valorHora * horas;
+
+    cout << fixed << setprecision(2);
+    cout << "O pagamento para " << nome << " deve ser R$ " << pagamento << endl;
+
+    return 0;
+}
