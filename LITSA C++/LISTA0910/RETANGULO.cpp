18f7e6ddc5e82e0958e167c40fb4948cc2e548b1
asaasnoturnas522-hash <asaasnoturnas522@gmail.com>
2025-10-14 18:14:44 -0300

    Create RETANGULO.cpp

---
+++ LITSA C++/LISTA0910/RETANGULO.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double base, altura, area, perimetro, diagonal;
+
+    cout << "Digite a base do retângulo: ";
+    cin >> base;
+    cout << "Digite a altura do retângulo: ";
+    cin >> altura;
+
+    area = base * altura;
+    perimetro = 2 * (base + altura);
+    diagonal = sqrt(pow(base, 2) + pow(altura, 2));
+
+    cout << fixed << setprecision(4);
+    cout << "Área = " << area << endl;
+    cout << "Perímetro = " << perimetro << endl;
+    cout << "Diagonal = " << diagonal << endl;
+
+    return 0;
+}
