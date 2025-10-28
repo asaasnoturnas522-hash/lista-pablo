a928584570aacbe37d6f0c7545b9369260277263
asaasnoturnas522-hash <asaasnoturnas522@gmail.com>
2025-10-28 17:50:20 -0300

    Create Inversão de um vetor.cpp

---
+++ LISTA C++2010/UNIDIMENSIONAIS/Inversão de um vetor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int vetor[10], inverso[10];
+    cout << "Digite 10 numeros:\n";
+    for(int i=0;i<10;i++)
+        cin >> vetor[i];
+
+    for(int i=0;i<10;i++)
+        inverso[i] = vetor[9 - i];
+
+    cout << "Vetor invertido:\n";
+    for(int i=0;i<10;i++)
+        cout << inverso[i] << " ";
+}
