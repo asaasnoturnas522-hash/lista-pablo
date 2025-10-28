5ec07c33959c256836814bc708d44501ab2adb5d
asaasnoturnas522-hash <asaasnoturnas522@gmail.com>
2025-10-28 17:50:45 -0300

    Create Sequência de Fibonacci.cpp

---
+++ LISTA C++2010/UNIDIMENSIONAIS/Sequência de Fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Quantos termos da sequencia de Fibonacci deseja ver? ";
+    cin >> n;
+
+    int a = 0, b = 1, c;
+    cout << a << " " << b << " ";
+    for(int i=2;i<n;i++) {
+        c = a + b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    }
+}
