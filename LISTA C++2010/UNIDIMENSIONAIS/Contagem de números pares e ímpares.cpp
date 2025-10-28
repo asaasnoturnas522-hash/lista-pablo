dfe392b0932c1ec94f98c0ae7658ebd0c37af8f6
asaasnoturnas522-hash <asaasnoturnas522@gmail.com>
2025-10-28 17:49:06 -0300

    Create Contagem de números pares e ímpares.cpp

---
+++ LISTA C++2010/UNIDIMENSIONAIS/Contagem de números pares e ímpares.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int vetor[20], pares = 0, impares = 0;
+    cout << "Digite 20 numeros inteiros:\n";
+    for(int i=0;i<20;i++) {
+        cin >> vetor[i];
+        if(vetor[i] % 2 == 0)
+            pares++;
+        else
+            impares++;
+    }
+    cout << "Pares: " << pares << "\nImpares: " << impares;
+}
